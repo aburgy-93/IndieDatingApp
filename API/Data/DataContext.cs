9dce02a863155243382c6a81e2e1093aed796003
aburgy-93 <aburgy@uwalumni.com>
2025-04-09 18:01:33 -0500

    Initial Commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+using System;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+}
