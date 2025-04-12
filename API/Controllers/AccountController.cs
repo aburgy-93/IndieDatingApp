d83d9f542cb5d9ca4d07c45d84fb9b19da546e8a
aburgy-93 <aburgy@uwalumni.com>
2025-04-12 18:19:06 -0500

    Added Login Logic

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -30,6 +30,29 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<User>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => 
+            x.Username == loginDto.Username.ToLower());
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        // Looping over the computed hash and the passwordHash saved in table
+        // If the symbol at position [i] of computedHash != the symbol at position [i] in user.passwordHash
+        // Then, the password is not correct, return Unathorized, else retun the user 
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await context.Users.AnyAsync(x => x.Username.ToLower() == username.ToLower()); // Bob != bob
